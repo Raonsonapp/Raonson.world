596a24b710340ef25323e98f13a8f62001a43e68
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-07 12:26:51 +0500

    Create Chatgpt_screen.dart

---
+++ lib/screens/Chatgpt_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatGPTScreen extends StatelessWidget {
+  const ChatGPTScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("ChatGPT 5 — Саволу ҷавоб"),
+      ),
+    );
+  }
+}
