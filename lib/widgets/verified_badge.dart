04ae039997d21b3af9834f976117580b2b4b9723
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-07 17:54:34 +0500

    Update verified_badge.dart

--- lib/widgets/verified_badge.dart
+++ lib/widgets/verified_badge.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
 
 class VerifiedBadge extends StatelessWidget {
-  const VerifiedBadge({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(2),
-      decoration: BoxDecoration(
-        color: Colors.green,
-        shape: BoxShape.circle,
-      ),
-      child: const Icon(Icons.check, color: Colors.white, size: 14),
-    );
+    return Icon(Icons.check_circle, color: Colors.green, size: 18);
   }
 }
