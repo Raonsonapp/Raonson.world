18a94c614b2e21b9237f6fd0c5b756b8520c125c
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-07 14:14:23 +0500

    Create bottom_nav.dart

---
+++ lib/ui/navigation/bottom_nav.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../pages/home/home_feed.dart';
+import '../pages/reels/reels_page.dart';
+import '../pages/search/search_page.dart';
+import '../pages/profile/profile_page.dart';
+import '../pages/notifications/notifications_page.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int index = 0;
+
+  final pages = const [
+    HomeFeed(),
+    SearchPage(),
+    ReelsPage(),
+    NotificationsPage(),
+    ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (i) => setState(() => index = i),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.play_arrow), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+        ],
+      ),
+    );
+  }
+}
